1d8be363d00907493f9326ac9ad6a62a0cf2f674
ysm95 <yahyaalnaimi.1995@gmail.com>
2023-08-30 09:48:15 +0400

    add route

--- routes/web.php
+++ routes/web.php
@@ -22,10 +22,17 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::post('invoice', [invoiceController::class, 'store'])->name('store');
+
+Route::get('/invoice', function () {
+    return view('invoice');
+})->middleware(['auth', 'verified'])->name('invoice');
+
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
